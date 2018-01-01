5f6e8871001c30915e211f6235c38f891b3b3d15
Carol Chen <carolyfisher@gmail.com>
2017-12-31 21:53:25 -0500

    N

--- table.py
+++ table.py
@@ -227,7 +227,7 @@ class TableItem(Gtk.EventBox):
             self.emit("mouse-enter")
 
     def __leave_cb(self, widget, event):
-        bg = self.get_property("background-color", Gtk.StateFlags.NORMAL)
+        # bg = self.get_property("background-color", Gtk.StateFlags.NORMAL)
         if self._current_color == self.color and bg != Color.GRAYED:
             self.modify_bg(Gtk.StateType.NORMAL, self.color)
             self.emit("mouse-leave")
